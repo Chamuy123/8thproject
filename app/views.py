90b9df1558ae49d7196caedb14da30c2dd241deb
Chamuy123 <yathamchamundeswari@gmail.com>
2023-12-04 21:23:29 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def data_render(request):
+    data='this data is our assumption'
+    d={'assumption':data}
+    return render(request,'data_render.html',context=d)
+
+
+def login(request):
+    d={'username':'chamu','age':18}
+    return render(request,'login.html',context=d)
